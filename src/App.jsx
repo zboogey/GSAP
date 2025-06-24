469cb477381ddbda4257c65a8e0c0fd1bce49837
Almat Ybray <you@example.com>
2025-06-24 20:41:17 +0500

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -15,6 +16,7 @@ const App = () => {
             <Hero />
             <Cocktails />
             <About />
+            <Art />
         </main>
     )
 }
